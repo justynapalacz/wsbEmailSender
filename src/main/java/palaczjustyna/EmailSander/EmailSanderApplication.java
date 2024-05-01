99b60b6631e57b791c660d6e7466b8393f1ffebc
Justyna Palacz <justynaguga@gmail.com>
2024-05-01 12:20:28 +0100

    Initial commit

---
+++ src/main/java/palaczjustyna/EmailSander/EmailSanderApplication.java
@@ -0,0 +1,13 @@
+package palaczjustyna.EmailSander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/palaczjustyna/EmailSander/EmailSanderApplicationTests.java
@@ -0,0 +1,13 @@
+package palaczjustyna.EmailSander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
